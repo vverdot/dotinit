675675b82c969e3469088e383ad283d5f82b16df
Vincent Verdot <vincent.verdot@gmail.com>
2017-07-04 14:58:47 +0200

    Updated dotinit

--- scripts/dotinit.sh
+++ scripts/dotinit.sh
@@ -1,3 +1,27 @@
 #!/bin/bash
 
-echo "hello world!"
+usage() {
+cat <<EOF
+Usage: dotinit [options] command ...
+
+Commands:
+    scan <dotfiles|packages>
+    	scan home directory dotfiles or installed packages
+
+    install [package.lst]
+    install packages from list (packages.lst by default)
+
+Options:
+    --help 	: display command usage
+    --dry-run 	: only simulate
+
+EOF
+}
+
+if [ $# -lt 1 ]; then
+	echo "dotinit:" invalid number of arguments
+	echo "Try 'dotinit --help' for more information."
+	exit 1
+fi
+
+usage
